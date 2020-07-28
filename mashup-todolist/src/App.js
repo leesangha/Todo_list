01092676b7c53767f0586fb8f9b0151286b59a66
leesangha <sanghalee1@naver.com>
2020-07-28 21:56:59 +0900

    Update App.js

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
 const GlobalStyle = createGlobalStyle`
 body{
@@ -14,6 +16,8 @@ function App() {
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
+        <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
